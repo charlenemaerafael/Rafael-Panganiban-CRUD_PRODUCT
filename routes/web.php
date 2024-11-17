30d97506b9b17272cf9ae6ddd88c8b9d4452ab84
Charlene Mae Rafael <you@example.com>
2024-11-17 15:30:10 +0800

    Fixed route for product delete action

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ Route::get("/{id}/show", [ProductController::class,"show"])->name("product.show"
 
 Route::get("/{id}/edit", [ProductController::class,"edit"])->name("product.edit");
 
-Route::put("/{id}", [ProductController::class,"update"])->name("product.update");
+Route::put("/product/{id}", [ProductController::class, "update"])->name("product.update");
 
 Route::delete("/product/{id}", [ProductController::class, "destroy"])->name("product.destroy");
